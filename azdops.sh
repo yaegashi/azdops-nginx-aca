4577ce7a5bdfa823dca4463813e2069a1482e764
YAEGASHI Takeshi <yaegashi@gmail.com>
2025-01-13 20:45:11 +0000

    Add GitHub workflows, azdops.sh, nginxacaops.sh

---
+++ azdops.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+
+set -e
+
+: ${NOPROMPT=false}
+
+unset CODESPACES
+unset GITHUB_TOKEN
+
+msg() {
+	echo ">>> $*" >&2
+}
+
+run() {
+	msg "Running: $@"
+	"$@"
+}
+
+confirm() {
+	if $NOPROMPT; then
+		return
+	fi
+	read -p ">>> Continue? [y/N] " -n 1 -r >&2
+	echo >&2
+	case "$REPLY" in
+		y) return ;;
+	esac
+	exit 1
+}
+
+enable_remote_env() {
+	msg 'Updating ~/.azd/config.yaml to enable the azd remote env'
+	confirm
+	run azd config set state.remote.backend AzureBlobStorage
+	run azd config set state.remote.config.accountName $1
+}
+
+disable_remote_env() {
+	msg 'Updating ~/.azd/config.yaml to disable the azd remote env'
+	confirm
+	run azd config unset state
+}
+
+cmd_auth_config() {
+	run azd config set auth.useAzCliAuth true
+}
+
+cmd_auth_az() {
+	run az login "$@"
+}
+
+cmd_auth_gh() {
+	run gh auth login "$@"
+}
+
+cmd_load() {
+	if test -z "$AZD_REMOTE_ENV_STORAGE_ACCOUNT_NAME" -o -z "$AZD_REMOTE_ENV_NAME"; then
+		BRANCH=$(git rev-parse --abbrev-ref HEAD)
+		AZD_REMOTE_FILE=".github/azdops/${BRANCH}/remote.yml"
+		if test -r "$AZD_REMOTE_FILE"; then
+			eval $(npx -y js-yaml "$AZD_REMOTE_FILE" | jq -r 'to_entries|map("\(.key)=\(.value)")|.[]')
+		fi
+	fi
+	if test -z "$AZD_REMOTE_ENV_STORAGE_ACCOUNT_NAME"; then
+		msg 'E: AZD_REMOTE_ENV_STORAGE_ACCOUNT_NAME is not set in the local env'
+		exit 1
+	fi
+	enable_remote_env $AZD_REMOTE_ENV_STORAGE_ACCOUNT_NAME
+	if test -n "$AZD_REMOTE_ENV_NAME"; then
+		run azd env select $AZD_REMOTE_ENV_NAME
+	fi
+	run azd env list
+}
+
+cmd_save() {
+	if ! eval $(azd env get-values); then
+		msg 'E: Failed to get values from the azd local env'
+		exit 1
+	fi
+	if test -z "$AZURE_STORAGE_ACCOUNT_NAME"; then
+		msg 'E: AZURE_STORAGE_ACCOUNT_NAME is not set in the azd local env'
+		exit 1
+	fi
+	enable_remote_env $AZURE_STORAGE_ACCOUNT_NAME
+	run azd env refresh
+	run azd env list
+}
+
+cmd_set() {
+	if ! eval $(azd env get-values); then
+		msg 'E: Failed to get values from the azd local env'
+		exit 1
+	fi
+	if test -z "$AZURE_STORAGE_ACCOUNT_NAME"; then
+		msg 'E: AZURE_STORAGE_ACCOUNT_NAME is not set in the azd local env'
+		exit 1
+	fi
+	run gh variable set AZD_REMOTE_ENV_NAME -b $AZURE_ENV_NAME
+	run gh variable set AZD_REMOTE_ENV_STORAGE_ACCOUNT_NAME -b $AZURE_STORAGE_ACCOUNT_NAME
+	run gh secret set AZD_REMOTE_ENV_NAME -b $AZURE_ENV_NAME -a codespaces
+	run gh secret set AZD_REMOTE_ENV_STORAGE_ACCOUNT_NAME -b $AZURE_STORAGE_ACCOUNT_NAME -a codespaces
+}
+
+cmd_clear() {
+	disable_remote_env
+	run azd env list
+}
+
+cmd_help() {
+	msg "Usage: $0 <command> [options...] [args...]"
+	msg "Options:"
+	msg "  --help,-h     - Show this help"
+	msg "  --no-prompt   - Do not ask for confirmation"
+	msg "Commands:"
+	msg "  auth-config   - Run \"azd config set auth.useAzCliAuth true\""
+	msg "  auth-az       - Run \"az login\""
+	msg "  auth-gh       - Run \"gh auth login\""
+	msg "  load          - Load the azd remote env"
+	msg "  save          - Save the azd remote env"
+	msg "  set           - Set GitHub secrets for the azd remote env"
+	msg "  clear         - Clear the azd remote env"
+	exit $1
+}
+
+OPTIONS=$(getopt -o h -l help,no-prompt -- "$@")
+if test $? -ne 0; then
+	cmd_help 1
+fi
+
+eval set -- "$OPTIONS"
+
+while true; do
+	case "$1" in
+		-h|--help)
+			cmd_help 0
+			;;
+		--no-prompt)
+			NOPROMPT=true
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			msg "E: Invalid option: $1"
+			cmd_help 1
+			;;
+	esac
+done
+
+if test $# -eq 0; then
+	msg "E: Missing command"
+	cmd_help 1
+fi
+
+case "$1" in
+	auth-config)
+		shift
+		cmd_auth_config "$@"
+		;;
+	auth-az)
+		shift
+		cmd_auth_az "$@"
+		;;
+	auth-gh)
+		shift
+		cmd_auth_gh "$@"
+		;;
+	load)
+		shift
+		cmd_load "$@"
+		;;
+	save)
+		shift
+		cmd_save "$@"
+		;;
+	set)
+		shift
+		cmd_set "$@"
+		;;
+	clear)
+		shift
+		cmd_clear "$@"
+		;;
+	*)
+		msg "E: Invalid command: $1"
+		cmd_help 1
+		;;
+esac
---
+++ nginxacaops.sh
@@ -0,0 +1,316 @@
+#!/bin/bash
+
+set -e
+
+eval $(azd env get-values)
+
+: ${NOPROMPT=false}
+: ${VERBOSE=false}
+: ${AZ_ARGS="-g $AZURE_RESOURCE_GROUP_NAME -n $AZURE_CONTAINER_APPS_APP_NAME"}
+: ${AZ_REVISION=}
+: ${AZ_REPLICA=}
+: ${AZ_CONTAINER=nginx}
+
+NL=$'\n'
+
+msg() {
+	echo ">>> $*" >&2
+}
+
+run() {
+   	msg "Running: $@"
+	"$@"
+}
+
+confirm() {
+	if $NOPROMPT; then
+		return
+	fi
+	read -p ">>> Continue? [y/N] " -n 1 -r >&2
+	echo >&2
+	case "$REPLY" in
+		[yY]) return
+	esac
+	exit 1
+}
+
+app_hostname() {
+	HOSTNAME=$(az containerapp hostname list $AZ_ARGS --query [0].name -o tsv)
+	if test -z "$HOSTNAME"; then
+		HOSTNAME=$(az containerapp show $ARGS --query properties.configuration.ingress.fqdn -o tsv)
+	fi
+	echo $HOSTNAME
+}
+
+cmd_meid_redirect() {
+	APP_HOSTNAME=$(app_hostname)
+	URI="https://${APP_HOSTNAME}/.auth/login/aad/callback"
+	URIS=$(az ad app show --id $MS_CLIENT_ID --query web.redirectUris -o tsv)
+	URIS=$(echo "${URI}${NL}${URIS}" | sort | uniq)
+	msg "ME-ID App Client ID:    ${MS_CLIENT_ID}"
+	msg "ME-ID App Redirect URI: ${URI}"
+	msg "Updating new Redirect URIs:${NL}${URIS}"
+	confirm
+	run az ad app update --id $MS_CLIENT_ID --web-redirect-uris $URIS
+}
+
+cmd_meid_secret() {
+	APP_HOSTNAME=$(app_hostname)
+	CRED_TIME=$(date +%s)
+	CRED_NAME="$APP_HOSTNAME $CRED_TIME"
+	msg "ME-ID App Client ID: ${MS_CLIENT_ID}"
+	msg "Adding new Client Secret for $APP_HOSTNAME"
+	confirm
+	msg "ME-ID App new credential name: $CRED_NAME"
+	PASSWORD=$(az ad app credential reset --id $MS_CLIENT_ID --append --display-name "$CRED_NAME" --end-date 2299-12-31 --query password -o tsv 2>/dev/null)
+	run az keyvault secret set --vault-name $AZURE_KEY_VAULT_NAME --name MS-CLIENT-SECRET --file <(echo -n "$PASSWORD") >/dev/null
+	run az containerapp revision copy $AZ_ARGS --revision-suffix $CRED_TIME >/dev/null
+}
+
+cmd_data_get() {
+	if test $# -lt 2; then
+		msg 'Specify remote/local paths'
+		exit 1
+	fi
+	run az storage file download --only-show-errors --account-name $AZURE_STORAGE_ACCOUNT_NAME -s data -p "$1" --dest "$2" >/dev/null
+}
+
+cmd_data_put() {
+	if test $# -lt 2; then
+		msg 'Specify remote/local paths'
+		exit 1
+	fi
+	run az storage file upload --only-show-errors --account-name $AZURE_STORAGE_ACCOUNT_NAME -s data -p "$1" --source "$2" >/dev/null
+}
+
+cmd_aca_show() {
+	run az containerapp show -g $AZURE_RESOURCE_GROUP_NAME -n $AZURE_CONTAINER_APPS_APP_NAME
+}
+
+cmd_aca_revisions() {
+	ARGS="$AZ_ARGS"
+	if ! $VERBOSE; then
+		ARGS="$ARGS --query [].{revision:name,created:properties.createdTime,state:properties.runningState,weight:properties.trafficWeight} -o table"
+	fi
+	run az containerapp revision list $ARGS
+}
+
+cmd_aca_replicas() {
+	ARGS="$AZ_ARGS"
+	if test -n "$AZ_REVISION"; then
+		ARGS="$ARGS --revision $AZ_REVISION"
+	fi
+	if ! $VERBOSE; then
+		ARGS="$ARGS --query [].{replica:name,created:properties.createdTime,state:properties.runningState} -o table"
+	fi
+	run az containerapp replica list $ARGS
+}
+
+cmd_aca_hostnames() {
+	ARGS="$AZ_ARGS"
+	if ! $VERBOSE; then
+		ARGS="$ARGS --query [].{hostname:name} -o table"
+	fi
+	run az containerapp hostname list $ARGS
+}
+
+cmd_aca_logs() {
+	ARGS="$AZ_ARGS --container $AZ_CONTAINER"
+	if test -n "$AZ_REVISION"; then
+		ARGS="$ARGS --revision $AZ_REVISION"
+	fi
+	if test -n "$AZ_REPLICA"; then
+		ARGS="$ARGS --replica $AZ_REPLICA"
+	fi
+	if ! $VERBOSE; then
+		ARGS="$ARGS --format text"
+	fi
+	if test "$1" = 'follow'; then
+		ARGS="$ARGS --follow"
+	fi
+	run az containerapp logs show $ARGS
+}
+
+cmd_aca_console() {
+	ARGS="$AZ_ARGS --container $AZ_CONTAINER"
+	if test -n "$AZ_REVISION"; then
+		ARGS="$ARGS --revision $AZ_REVISION"
+	fi
+	if test -n "$AZ_REPLICA"; then
+		ARGS="$ARGS --replica $AZ_REPLICA"
+	fi
+	CMD="$*"
+	if test -z "$CMD"; then
+		CMD=bash
+	fi
+	run az containerapp exec $ARGS --command "$CMD"
+	run stty sane
+}
+
+cmd_aca_restart() {
+	if test -z "$AZ_REVISION"; then
+		AZ_REVISION=$(az containerapp show -g $AZURE_RESOURCE_GROUP_NAME -n $AZURE_CONTAINER_APPS_APP_NAME --query properties.latestRevisionName -o tsv)
+	fi
+	ARGS="$AZ_ARGS --revision $AZ_REVISION"
+	msg "Restarting revision $AZ_REVISION..."
+	confirm
+	run az containerapp revision restart $ARGS
+}
+
+cmd_portal_aca() {
+	URL="https://portal.azure.com/#@${AZURE_TENANT_ID}/resource/subscriptions/${AZURE_SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}"
+	run xdg-open "$URL"
+}
+
+cmd_portal_meid() {
+	if test -z "$MS_TENANT_ID" -o -z "$MS_CLIENT_ID"; then
+		msg 'Missing MS_TEANT_ID or MS_CLIENT_ID settings'
+		exit 1
+	fi
+	URL="https://portal.azure.com/#@${MS_TENANT_ID}/view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/${MS_CLIENT_ID}"
+	run xdg-open "$URL"
+}
+
+cmd_open() {
+	HOSTNAME=$(app_hostname)
+  	URL="https://${HOSTNAME}${APP_ROOT_PATH}"
+	run xdg-open "$URL"
+}
+
+cmd_help() {
+	msg "Usage: $0 <command> [options...] [args...]"
+	msg "Options":
+	msg "  --help,-h                  - Show this help"
+	msg "  --no-prompt                - Do not ask for confirmation"
+	msg "  --verbose, -v              - Show detailed output"
+	msg "  --revision <name>          - Specify revision name"
+	msg "  --replica <name>           - Specify replica name"
+	msg "  --container <name>         - Specify container name"
+	msg "Commands:"
+	msg "  meid-redirect              - ME-ID: update app redirect URIs"
+	msg "  meid-secret                - ME-ID: create new client secret"
+	msg "  data-get <remote> <local>  - Data: download file"
+	msg "  data-put <remote> <local>  - Data: upload file"
+	msg "  aca-show                   - ACA: show app"
+	msg "  aca-revisions              - ACA: list revisions"
+	msg "  aca-replicas               - ACA: list replicas"
+	msg "  aca-hostnames              - ACA: list hostnames"
+	msg "  aca-restart                - ACA: restart revision"
+	msg "  aca-logs [follow]          - ACA: show container logs"
+	msg "  aca-console [command...]   - ACA: connect to container"
+	msg "  portal-aca                 - Portal: open ACA resource group in browser"
+	msg "  portal-meid                - Portal: open ME-ID app registration in browser"
+	msg "  open                       - open app in browser"
+	exit $1
+}
+
+OPTIONS=$(getopt -o hqv -l help -l no-prompt -l verbose -l revision: -l replica: -l container: -- "$@")
+if test $? -ne 0; then
+	cmd_help 1
+fi
+
+eval set -- "$OPTIONS"
+
+while true; do
+	case "$1" in
+		-h|--help)
+			cmd_help 0
+			;;			
+		--no-prompt)
+			NOPROMPT=true
+			shift
+			;;
+		-v|--verbose)
+			VERBOSE=true
+			shift
+			;;
+		--revision)
+			AZ_REVISION=$2
+			shift 2
+			;;
+		--replica)
+			AZ_REPLICA=$2
+			shift 2
+			;;
+		--container)
+			AZ_CONTAINER=$2
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			msg "E: Invalid option: $1"
+			cmd_help 1
+			;;
+	esac
+done
+
+if test $# -eq 0; then
+	msg "E: Missing command"
+	cmd_help 1
+fi
+
+case "$1" in
+	meid-redirect)
+		shift
+		cmd_meid_redirect "$@"
+		;;
+	meid-secret)
+		shift
+		cmd_meid_secret "$@"
+		;;
+	data-get|download)
+		shift
+		cmd_data_get "$@"
+		;;
+	data-put|upload)
+		shift
+		cmd_data_put "$@"
+		;;
+	aca-show)
+		shift
+		cmd_aca_show "$@"
+		;;
+	aca-revisions)
+		shift
+		cmd_aca_revisions "$@"
+		;;
+	aca-replicas)
+		shift
+		cmd_aca_replicas "$@"
+		;;
+	aca-hostnames)
+		shift
+		cmd_aca_hostnames "$@"
+		;;
+	aca-logs)
+		shift
+		cmd_aca_logs "$@"
+		;;
+	aca-console)
+		shift
+		cmd_aca_console "$@"
+		;;
+	aca-restart)
+		shift
+		cmd_aca_restart "$@"
+		;;
+	portal-aca)
+		shift
+		cmd_portal_aca "$@"
+		;;
+	portal-meid)
+		shift
+		cmd_portal_meid "$@"
+		;;
+	open)
+		shift
+		cmd_open "$@"
+		;;
+	*)
+		msg "E: Invalid command: $1"
+		cmd_help 1
+		;;
+esac
